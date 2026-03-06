6be92ffe7d7f374d34cdbec8bfde42a57094be4f
Joe Dickinson <josephd850@gmail.com>
2026-03-06 17:46:01 +0000

    Add multiplayer authority to worm player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+func _enter_tree() -> void:
+	set_multiplayer_authority(int(str(name)))
+
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_direction * speed
 
 func _physics_process(_delta):
+	if !is_multiplayer_authority():
+		return
 	get_input()
 	move_and_slide()
