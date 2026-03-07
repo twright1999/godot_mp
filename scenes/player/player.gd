aa5d304f10dcc7c8cb77fc464ae1617ea6c34aee
Joe Dickinson <josephd850@gmail.com>
2026-03-07 15:52:18 +0000

    Changed spawn position of worms (had trouble setting spawn position when adding as child to world). Disabled collision between each other.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 func _enter_tree() -> void:
 	# Peer id must match player name in order to control
 	set_multiplayer_authority(int(str(name)))
+	global_position = get_viewport_rect().size / 2
 
 func get_input():
 	var input_direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
